51d5457cfb2c6cadcec607e935ddefc179ea53e3
Neeraj Lakshmanan <neerajsubi@gmail.com>
2025-04-15 20:00:07 +0800

    Added setup file for virtual env

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Create the virtual environment
+python3 -m venv myenv
+
+# Activate the virtual environment
+source myenv/bin/activate
+
+# Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+echo "Environment setup complete!"
